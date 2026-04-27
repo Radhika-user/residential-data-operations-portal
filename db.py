1e8040a5704385030191fb6c6febf04ddb6fbfb8
Radhika-user <radhikaj@POCONNOR.com>
2026-04-27 13:09:57 +0530

    db.py

--- db.py
+++ db.py
@@ -1,33 +1,37 @@
 import pyodbc
+import platform
 
 def base_conn(db):
-    print("Available drivers:", pyodbc.drivers())
-
     drivers = [
         "ODBC Driver 18 for SQL Server",
         "ODBC Driver 17 for SQL Server",
         "ODBC Driver 13 for SQL Server"
     ]
 
+    server = "fileprepdb"
     last_error = None
+
+    if platform.system() != "Windows":
+        raise Exception("Windows Authentication only works on Windows machines")
 
     for driver in drivers:
         try:
             conn_str = (
                 f"DRIVER={{{driver}}};"
-                f"SERVER=fileprepdb;"
+                f"SERVER={server};"
                 f"DATABASE={db};"
                 f"Trusted_Connection=yes;"
                 f"TrustServerCertificate=yes;"
             )
+
             print(f"Trying {driver}")
             return pyodbc.connect(conn_str)
 
         except Exception as e:
-            print(f"{driver} failed -> {e}")
+            print(f"{driver} failed: {e}")
             last_error = e
 
-    raise Exception(f"All ODBC drivers failed: {last_error}")
+    raise Exception(f"No working ODBC driver found: {last_error}")
 
 
 def get_csdb_connection():
