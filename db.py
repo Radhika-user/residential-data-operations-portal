269962da8ff278893f9475dd5bd9c2a2cff2f27f
Radhika-user <radhikaj@POCONNOR.com>
2026-04-28 10:31:41 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,37 +1,20 @@
 import pyodbc
 import os
-
+ 
 def base_conn(db):
     server = os.getenv("DB_SERVER", "fileprepdb")
-
-    drivers = [
-        "ODBC Driver 18 for SQL Server"
-    ]
-
-    last_error = None
-
-    for driver in drivers:
-        try:
-            conn = pyodbc.connect(
-                "DRIVER={" + driver + "};"
-                f"SERVER={server};"
-                f"DATABASE={db};"
-                "Trusted_Connection=yes;"
-                "TrustServerCertificate=yes;"
-            )
-            print(f"Connected using {driver}")
-            return conn
-
-        except Exception as e:
-            last_error = e
-            print(f"{driver} failed: {e}")
-
-    raise Exception(f"No ODBC driver worked. Last error: {last_error}")
-
-
+ 
+    conn = pyodbc.connect(
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER={server};"
+        f"DATABASE={db};"
+        "Trusted_Connection=yes;"
+        "TrustServerCertificate=yes;"
+    )
+    return conn
+ 
 def get_csdb_connection():
     return base_conn("CSDB")
-
-
+ 
 def get_taxroll_connection():
     return base_conn("CSDBTaxroll")
