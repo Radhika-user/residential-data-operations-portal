2ebd3d83e064b7c087fab87522e6504ed0f9517a
Radhika-user <radhikaj@POCONNOR.com>
2026-04-28 12:09:24 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,21 +1,21 @@
 import pyodbc
 import os
- 
+
 def base_conn(db):
     server = os.getenv("DB_SERVER", "fileprepdb")
- 
+
     conn = pyodbc.connect(
         "DRIVER={ODBC Driver 18 for SQL Server};"
         f"SERVER={server};"
         f"DATABASE={db};"
         "Trusted_Connection=yes;"
         "TrustServerCertificate=yes;"
+        "Connection Timeout=3000;"
     )
     return conn
- 
+
 def get_csdb_connection():
     return base_conn("CSDB")
- 
+
 def get_taxroll_connection():
     return base_conn("CSDBTaxroll")
- Connection Timeout=15;
